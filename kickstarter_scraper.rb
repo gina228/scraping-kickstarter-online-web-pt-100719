33e00d89d605147484ae01bd932859090af55678
Gina Graci <ginagraci92@gmail.com>
2019-11-14 01:32:01 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -14,13 +14,7 @@ def create_project_hash
   
   projects = {}
  
-  # Iterate through the projects
-  kickstarter.css("li.project.grid_4").each do |project|
-    projects[project] = {}
-  end
- 
-  # return the projects hash
-  projects
+  
 end
 
 create_project_hash
